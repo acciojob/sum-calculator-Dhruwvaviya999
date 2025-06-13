14c8bc9bb009e7d470b080309334dc9237d303e1
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-06-13 16:05:57 +0530

    Create SumCalculator.js

---
+++ src/components/SumCalculator.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+
+const SumCalculator = () => {
+
+    const [inputValue, setInputValue] = useState('');
+    const [sum, setSum] = useState(0);
+    const [numbers, setNumbers] = useState([]);
+
+    useEffect(() => {
+        let isCancelled = false;
+
+    const calculateSum = async () => {
+      await new Promise(resolve => setTimeout(resolve, 100));
+
+      if (!isCancelled) {
+        const total = numbers.reduce((acc, num) => acc + num, 0);
+        setSum(total);
+      }
+    };
+
+    calculateSum();
+
+    return () => {
+      isCancelled = true; // prevent memory leaks
+    };
+    }, [numbers])
+
+
+  return (
+    <div>
+        <input value={inputValue} onChange={(e) => {
+            setInputValue(e.target.value);
+             const num = parseInt(inputValue);
+            if (!isNaN(num)) {
+            setNumbers(prev => [...prev, num]); // add number to the list
+            setInputValue(''); // clear input
+            }
+        }} style={{width: '300px', height: '30px'}} placeholder='Enter Any Number' type="number" id='number' />
+        <p>Sum: {sum}</p>
+    </div>
+  )
+}
+
+export default SumCalculator
